74a64f438d300a75f3730e4e2e8171f95a4d5f35
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2021-10-13 21:34:43 +0700

    Update data-kategori.php

--- riskyCakes/data-kategori.php
+++ riskyCakes/data-kategori.php
@@ -37,7 +37,7 @@
                             <tr>
                                 <th>No</th>
                                 <th>Kategori</th>
-                                <th>Aks</th>
+                                <th>Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
